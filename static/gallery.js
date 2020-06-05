5cc44ad652474eb8fea73fbfff05e944792da80e
tbriggs1 <tombriggs93@protonmail.com>
2020-06-05 14:21:25 +0100

    pushed_to_aws

---
+++ static/gallery.js
@@ -0,0 +1,11 @@
+let age = prompt("Please enter your age");
+
+if(age < 0){
+    alert("Wow no getting it here mate")
+}
+else if(age == 21){
+    alert("Happy bday")
+}
+else if(age % 2 == 1){
+    alert("Wow odd ball")
+}
